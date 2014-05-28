9bac495a5c367b6bf9e7be13253ac5e70cb9b391
Matt Walker <mwalker@khaosdev.com>
2014-05-27 21:14:46 -0700

    Updating configuration to point to absolute puppet paths
    Change-Id: Ic79e3f22fba659aff37a4879cd8bb33b0bb9252f

--- LocalSettings.js
+++ LocalSettings.js
@@ -2,6 +2,9 @@ module.exports = function (config) {
 	config.coordinator.frontend_threads = 2;
 	config.coordinator.backend_threads = "auto";
 
+	config.backend.bundler.bin = "/srv/deployment/ocg/ocg/mw-ocg-bundler/bin/mw-ocg-bundler";
+	config.backend.writers.rdf2latex.bin = "/srv/deployment/ocg/ocg/ocg-collection/mw-ocg-latexer/bin/mw-ocg-latexer";
+
 	return config;
 }
 
